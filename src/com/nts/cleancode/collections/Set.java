963c9995420f2e3972a94e22782dfb770721f0df
Larva <ssh9112@naver.com>
2016-06-08 21:05:23 +0900

    Refactoring Problem (not condition)

--- src/com/nts/cleancode/collections/Set.java
+++ src/com/nts/cleancode/collections/Set.java
@@ -2,19 +2,20 @@ package com.nts.cleancode.collections;
 
 public class Set extends AbstractCollection {
 	public void add(Object element) {
-		if (!readOnly) {
-			int newSize = size + 1;
-			if (newSize > elements.length) {
-				Object[] newElements =
+		if (readOnly) 
+			return;
+		
+		int newSize = size + 1;
+		if (newSize > elements.length) {
+			Object[] newElements =
 					new Object[elements.length + INITIAL_CAPACITY];
-				for (int i = 0; i < size; i++)
-					newElements[i] = elements[i];
-				elements = newElements;
-			}
-
-			if (contains(element))
-				return;
-			elements[size++] = element;
+			for (int i = 0; i < size; i++)
+				newElements[i] = elements[i];
+			elements = newElements;
 		}
+		
+		if (contains(element))
+			return;
+		elements[size++] = element;
 	}
 }
